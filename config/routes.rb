2858b2cbdded98c7f6ff38eb6d69c6d3d851eccf
soraya-ire <rujiangkongshi@gmail.com>
2024-07-25 13:11:46 +0900

    feat: ログイン時のヘッダーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'users#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
