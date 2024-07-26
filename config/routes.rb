e57be0db38f25d2561059014563832e0383198c1
soraya-ire <rujiangkongshi@gmail.com>
2024-07-26 14:33:57 +0900

    feat: ゲストログイン用のルーティングを用意

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
 end
