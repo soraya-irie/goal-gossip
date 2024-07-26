d07c9e1caa530adc35dd0e51fa713afdbbf8db0d
soraya-ire <rujiangkongshi@gmail.com>
2024-07-26 14:39:16 +0900

    feat: guestクラスメソッドを作成し、コントローラを簡潔にする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  GUEST_USER_EMAIL = "guest@example.com"
+  USER_NAME = "ゲスト"
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = USER_NAME
+    end
+  end
 end
