a9cab3e2d39c7ea4cb53386d6b605f2680c12f33
soraya-ire <rujiangkongshi@gmail.com>
2024-08-02 15:35:47 +0900

    feat: Postsコントローラの雛形を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: '投稿が保存されました'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: '投稿が更新されました'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: '投稿が削除されました'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:user_id, :stadium_name, :address, :match_date, :supported_team, :comment)
+  end
 end
