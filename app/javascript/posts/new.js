68815178dc8ef64479c299c3279867ca567d7f51
soraya-ire <rujiangkongshi@gmail.com>
2024-08-04 23:19:06 +0900

    feat: htmlのscriptタグのコードを移す

---
+++ app/javascript/posts/new.js
@@ -0,0 +1,43 @@
+document.addEventListener('turbo:load', () => {
+  const mapElement = document.getElementById('map');
+  const searchButton = document.querySelector('.search-form-button');
+  const nameDis = document.getElementById('post_stadium_name');
+  const addressDis = document.getElementById('post_address');
+  const addressInput = document.getElementById('address');
+  let map, marker, geocoder;
+
+  function initMap() {
+    const nationalStadium = { lat: 35.677824, lng: 139.714541 };
+    map = new google.maps.Map(mapElement, {
+      center: nationalStadium,
+      zoom: 14,
+    });
+    marker = new google.maps.Marker({ position: nationalStadium, map });
+    geocoder = new google.maps.Geocoder();
+  }
+
+  function codeAddress() {
+    const address = addressInput.value;
+    geocoder.geocode({ address }, (results, status) => {
+      if (status === 'OK' && results[0]) {
+        const location = results[0].geometry.location;
+        map.setCenter(location);
+        marker.setPosition(location);
+        nameDis.value = address;
+        addressDis.value = results[0].formatted_address;
+      } else {
+        alert(`該当する結果がありませんでした：${status}`);
+      }
+    });
+  }
+
+  function addEventListeners() {
+    searchButton.addEventListener('click', codeAddress);
+  }
+
+  initMap();
+  addEventListeners();
+});
+
+window.initMap = initMap;
+window.codeAddress = codeAddress;
