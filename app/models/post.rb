6207f646cef3d11d13899e363de51ea5d8b72eeb
soraya-ire <rujiangkongshi@gmail.com>
2024-08-04 13:32:19 +0900

    feat: 今日以降の試合の感想を投稿できないように

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, :stadium_name, :address, :match_date, :supported_team, :comment, presence: true
+  validate :validate_not_future_date
+
+  private
+
+  def validate_not_future_date
+    if match_date.present? && match_date > Date.today
+      errors.add(:match_date, ":今日以降の日付は選択できません。")
+    end
+  end
 end
