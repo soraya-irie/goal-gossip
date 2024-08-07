4f6f3580035fe57073127043b8e60bdce5f33515
soraya-ire <rujiangkongshi@gmail.com>
2024-08-07 13:51:04 +0900

    feat: destroyアクションにstatusが必要が必要

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
-    redirect_to posts_url, notice: '投稿が削除されました'
+    redirect_to root_path, notice: '投稿が削除されました', status: :see_other
   end
 
   private
