97a9a5cebf29e49fb6adfd8069c2725f8d2890c7
soraya-ire <rujiangkongshi@gmail.com>
2024-08-08 11:27:35 +0900

    chore: ログインフォームとサインアップフォームにアクセスできるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users, only: [:show]
   resources :posts, except: [:index]
   devise_for :users, controllers: {
     registrations: 'users/registrations',
@@ -10,4 +9,5 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
+  resources :users, only: [:show]
 end
