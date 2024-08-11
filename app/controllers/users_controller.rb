a9f1afc50e28661a01c1de7262bed1dd6c4f0f63
soraya-ire <rujiangkongshi@gmail.com>
2024-08-11 14:23:14 +0900

    feat: ページネーションを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @posts = Post.includes(:user).order(created_at: :desc)
+    @pagy, @posts = pagy(Post.includes(:user).order(created_at: :desc))
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order(created_at: :desc)
+    @pagy, @posts = pagy(@user.posts.order(created_at: :desc))
   end
 end
