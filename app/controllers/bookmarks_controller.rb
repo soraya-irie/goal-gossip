3acea69f6242cf8bf0ab13d3d1a7e22c67f9c746
soraya-ire <rujiangkongshi@gmail.com>
2024-08-13 15:00:39 +0900

    feat: Bookmarksコントローラのメソッドを作成

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,15 @@
 class BookmarksController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    current_user.bookmark(@post)
+    flash[:notice] = "ブックマークしました"
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @post = current_user.bookmarks.find(params[:id]).post
+    current_user.unbookmark(@post)
+    flash[:notice] = "ブックマークを解除しました"
+    redirect_back(fallback_location: root_path)
+  end
 end
