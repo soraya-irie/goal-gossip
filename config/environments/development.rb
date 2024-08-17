8c6b379426054ae7934cdeeae1c4de3d3ba5d823
soraya-ire <rujiangkongshi@gmail.com>
2024-08-17 16:27:46 +0900

    chore: デプロイ時のblocked hostエラーを解消

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "goal-gosiip-e2571a0eef11.herokuapp.com"
 end
