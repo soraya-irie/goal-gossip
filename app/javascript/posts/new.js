e23735786ee9a03a3a45af96bd19f98ea5c53617
soraya-ire <rujiangkongshi@gmail.com>
2024-08-19 14:37:46 +0900

    feat: initializeメソッドを消去

--- app/javascript/posts/new.js
+++ app/javascript/posts/new.js
@@ -1,42 +1,40 @@
-document.addEventListener('turbo:load', initialize);
-document.addEventListener('turbo:render', initialize);
+document.addEventListener('turbo:load', initMap);
+document.addEventListener('turbo:render', initMap);
 
-function initialize() {
+let map, geocoder;
+
+function initMap() {
   const mapElement = document.getElementById('map');
   const searchButton = document.querySelector('.search-form-button');
   const nameDis = document.getElementById('post_stadium_name');
   const addressDis = document.getElementById('post_address');
   const addressInput = document.getElementById('address');
-  let map, geocoder;
 
-  function initMap() {
-    const nationalStadium = { lat: 35.677824, lng: 139.714541 };
-    map = new google.maps.Map(mapElement, {
-      center: nationalStadium,
-      zoom: 18,
-      mapTypeId: 'satellite',
-    });
-    geocoder = new google.maps.Geocoder();
-  }
+  const nationalStadium = { lat: 35.677824, lng: 139.714541 };
+  map = new google.maps.Map(mapElement, {
+    center: nationalStadium,
+    zoom: 18,
+    mapTypeId: 'satellite',
+  });
+  geocoder = new google.maps.Geocoder();
 
-  function codeAddress() {
-    const address = addressInput.value;
-    geocoder.geocode({ address }, (results, status) => {
-      if (status === 'OK' && results[0]) {
-        const location = results[0].geometry.location;
-        map.setCenter(location);
-        nameDis.value = address;
-        addressDis.value = results[0].formatted_address;
-      } else {
-        alert(`該当する結果がありませんでした：${status}`);
-      }
-    });
-  }
+  addEventListeners(searchButton, addressInput, nameDis, addressDis);
+}
 
-  function addEventListeners() {
-    searchButton.addEventListener('click', codeAddress);
-  }
+function codeAddress(addressInput, nameDis, addressDis) {
+  const address = addressInput.value;
+  geocoder.geocode({ address }, (results, status) => {
+    if (status === 'OK' && results[0]) {
+      const location = results[0].geometry.location;
+      map.setCenter(location);
+      nameDis.value = address;
+      addressDis.value = results[0].formatted_address;
+    } else {
+      alert(`該当する結果がありませんでした：${status}`);
+    }
+  });
+}
 
-  initMap();
-  addEventListeners();
+function addEventListeners(searchButton, addressInput, nameDis, addressDis) {
+  searchButton.addEventListener('click', () => codeAddress(addressInput, nameDis, addressDis));
 }
