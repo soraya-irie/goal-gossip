5e1c7962c67fe779a8e6e993f87508236fc54ec5
soraya-ire <rujiangkongshi@gmail.com>
2024-08-19 12:51:14 +0900

    feat: headerのjavascriptの記述場所を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', function() {
+  const hamburger = document.querySelectorAll('#hamburger, .close');
+  const nav = document.querySelector('.sp-nav');
+
+  hamburger.forEach(element => {
+    element.addEventListener('click', function() {
+      nav.classList.toggle('toggle');
+    });
+  });
+});
