989c0cc1dd56e8462787fbd7f808bd5fed76ec4f
soraya-ire <rujiangkongshi@gmail.com>
2024-08-23 17:56:45 +0900

    test: Userモデルのメソッドのテストを追加

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'user_idがpost_idに対して一意であることを検証する' do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post)
+    FactoryBot.create(:bookmark, user: user, post: post)
+    duplicate_bookmark = FactoryBot.build(:bookmark, user: user, post: post)
+
+    expect(duplicate_bookmark).not_to be_valid
+    expect(duplicate_bookmark.errors[:user_id]).to include('はすでに存在します')
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "nameがあれば有効であること" do
-    user = FactoryBot.build(:user)
-    expect(user).to be_valid
+  describe 'validations' do
+    it "nameがあれば有効であること" do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
+
+    it "nameがなければ無効なこと" do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it "nameが15文字以内であれば有効なこと" do
+      user = FactoryBot.build(:user, name: "a" * 15)
+      expect(user).to be_valid
+    end
+
+    it "nameが16文字以上であれば無効であること" do
+      user = FactoryBot.build(:user, name: "a" * 16)
+      user.valid?
+      expect(user.errors[:name]).to include("は15文字以内で入力してください")
+    end
   end
 
-  it "nameがなければ無効なこと" do
-    user = FactoryBot.build(:user, name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("を入力してください")
-  end
+  describe 'methods' do
+    it "User.guestメソッドがゲストユーザーを返すこと" do
+      user = User.guest
+      expect(user.name).to eq("ゲスト")
+      expect(user.email).to eq("guest@example.com")
+      expect(user.password).to be_present
+    end
 
-  it "nameが15文字以内であれば有効なこと" do
-    user = FactoryBot.build(:user, name: "a" * 15)
-    expect(user).to be_valid
-  end
+    before do
+      @user = FactoryBot.create(:user)
+      @post = FactoryBot.create(:post)
+    end
 
-  it "nameが16文字以上であれば無効であること" do
-    user = FactoryBot.build(:user, name: "a" * 16)
-    user.valid?
-    expect(user.errors[:name]).to include("は15文字以内で入力してください")
-  end
+    it "bookmarkメソッドがブックマークを追加すること" do
+      @user.bookmark(@post)
+      expect(@user.bookmarks.count).to eq(1)
+    end
 
-  it "User.guestメソッドがゲストユーザーを返すこと" do
-    user = User.guest
-    expect(user.name).to eq("ゲスト")
-    expect(user.email).to eq("guest@example.com")
-    expect(user.password).to be_present
-  end
+    it "unbookmarkメソッドがブックマークを削除すること" do
+      @user.bookmark(@post)
+      @user.unbookmark(@post)
+      expect(@user.bookmarks.count).to eq(0)
+    end
 
-  it "bookmarkメソッドがブックマークを追加すること"
-  it "unbookmarkメソッドがブックマークを削除すること"
-  it "bookmarked?メソッドがブックマークされているかどうかを返すこと"
-  it "own?メソッドがオブジェクトのユーザーIDと一致するかどうかを返すこと"
+    it "bookmarked?メソッドがブックマークされているかどうかを返すこと" do
+      @user.bookmark(@post)
+      expect(@user.bookmarked?(@post)).to be_truthy
+    end
+
+    it "own?メソッドがオブジェクトのユーザーIDと一致するかどうかを返すこと" do
+      @post = FactoryBot.create(:post, user: @user)
+      expect(@user.own?(@post)).to be_truthy
+    end
+  end
 end
