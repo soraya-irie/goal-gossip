b33aa54f1dafd1acf79711fc199de1de33965da3
soraya-ire <rujiangkongshi@gmail.com>
2024-08-25 19:39:29 +0900

    test: system/bookmarks_specを追加

--- spec/system/bookmarks_spec.rb
+++ spec/system/bookmarks_spec.rb
@@ -1,9 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "Bookmarks", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  it "ユーザーがブックマークを追加、解除する" do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post)
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    visit root_path
+    click_link "ログインフォームへ"
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログインする"
+
+    expect {
+      find('.bookmark-icon').click
+      expect(page).to have_content "ブックマークしました"
+    }.to change { Bookmark.count }.by(1)
+
+    visit bookmarks_posts_path
+    expect(page).to have_content post.stadium_name
+    expect(page).to have_content post.match_date.strftime("%m/%d")
+    expect(page).to have_content post.supported_team
+    expect(page).to have_content post.comment
+
+    visit root_path
+    find('.unbookmark-icon').click
+    expect(page).to have_content "ブックマークを解除しました"
+
+    visit bookmarks_posts_path
+    expect(page).not_to have_content post.stadium_name
+    expect(page).not_to have_content post.match_date.strftime("%m/%d")
+    expect(page).not_to have_content post.supported_team
+    expect(page).not_to have_content post.comment
+  end
 end
