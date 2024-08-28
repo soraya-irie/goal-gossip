ac9524c4c7d9e53897ec9ec496b3a30f4e5b1b0a
soraya-ire <rujiangkongshi@gmail.com>
2024-08-28 15:24:10 +0900

    feat: バリデーションエラー後に、Mapの場所が変わらないようにする

--- app/javascript/posts/new.js
+++ app/javascript/posts/new.js
@@ -5,36 +5,40 @@ let map, geocoder;
 
 function initMap() {
   const mapElement = document.getElementById('map');
-  const searchButton = document.querySelector('.search-form-button');
-  const nameDis = document.getElementById('post_stadium_name');
-  const addressDis = document.getElementById('post_address');
-  const addressInput = document.getElementById('address');
-
-  const nationalStadium = { lat: 35.677824, lng: 139.714541 };
   map = new google.maps.Map(mapElement, {
-    center: nationalStadium,
     zoom: 18,
     mapTypeId: 'satellite',
   });
   geocoder = new google.maps.Geocoder();
+  clickSearchButton();
 
-  addEventListeners(searchButton, addressInput, nameDis, addressDis);
+  if (document.getElementById('post_stadium_name').value) {
+    geocoder.geocode({ address: document.getElementById('post_stadium_name').value }, (results, status) => {
+      if (status === 'OK' && results[0]) {
+        const location = results[0].geometry.location;
+        map.setCenter(location);
+      } else {
+        alert(`該当する結果がありませんでした：${status}`);
+      }
+    });
+  }
 }
 
-function codeAddress(addressInput, nameDis, addressDis) {
-  const address = addressInput.value;
+function clickSearchButton() {
+  const searchButton = document.querySelector('.search-form-button');
+  searchButton.addEventListener('click', () => codeAddress());
+}
+
+function codeAddress() {
+  const address = document.getElementById('address').value;
   geocoder.geocode({ address }, (results, status) => {
     if (status === 'OK' && results[0]) {
       const location = results[0].geometry.location;
       map.setCenter(location);
-      nameDis.value = address;
-      addressDis.value = results[0].formatted_address;
+      document.getElementById('post_stadium_name').value = address;
+      document.getElementById('post_address').value = results[0].formatted_address;
     } else {
       alert(`該当する結果がありませんでした：${status}`);
     }
   });
-}
-
-function addEventListeners(searchButton, addressInput, nameDis, addressDis) {
-  searchButton.addEventListener('click', () => codeAddress(addressInput, nameDis, addressDis));
 }
