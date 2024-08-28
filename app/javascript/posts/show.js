bf7361b63466926aea6825e715444c19b4f635c6
soraya-ire <rujiangkongshi@gmail.com>
2024-08-28 15:27:42 +0900

    feat: initMapを一箇所にまとめる

--- app/javascript/posts/show.js
+++ app/javascript/posts/show.js
@@ -1,24 +1,23 @@
-document.addEventListener('turbo:load', () => {
-  function initMap() {
-    const address = document.getElementById('post_stadium_name').value;
-    const geocoder = new google.maps.Geocoder();
+document.addEventListener('turbo:load', initMap);
+document.addEventListener('turbo:render', initMap);
 
-    geocoder.geocode({ address }, (results, status) => {
-      if (status === 'OK' && results[0]) {
-        const location = results[0].geometry.location;
-        const mapOptions = {
-          center: location,
-          zoom: 18,
-          mapTypeId: 'satellite'
-        };
+function initMap() {
+  const stadiumName = document.getElementById('post_stadium_name').value;
+  const geocoder = new google.maps.Geocoder();
 
-        const mapElement = document.getElementById('map');
-        new google.maps.Map(mapElement, mapOptions);
-      } else {
-        console.error(`ジオコーディングが次の理由で失敗しました: ${status}`);
-      }
-    });
-  }
+  geocoder.geocode({ address: stadiumName }, (results, status) => {
+    if (status === 'OK' && results[0]) {
+      const location = results[0].geometry.location;
+      const mapOptions = {
+        center: location,
+        zoom: 18,
+        mapTypeId: 'satellite'
+      };
 
-  initMap();
-});
+      const mapElement = document.getElementById('map');
+      new google.maps.Map(mapElement, mapOptions);
+    } else {
+      console.error(`ジオコーディングが次の理由で失敗しました: ${status}`);
+    }
+  });
+}
