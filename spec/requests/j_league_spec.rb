74ccdb8f170ac39e9b5f01858391e77a069974d1
soraya-ire <rujiangkongshi@gmail.com>
2024-09-09 15:06:40 +0900

    test: /j_leagueにアクセスできることをテスト

--- spec/requests/j_league_spec.rb
+++ spec/requests/j_league_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "JLeagues", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /j_league" do
+    it "/j_league/standingsにアクセスする" do
+      get j_league_standings_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "/j_league/teamsにアクセスする" do
+      get j_league_teams_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "/j_league/searchにアクセスする" do
+      get j_league_search_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it "/j_league/:idにアクセスする" do
+      get team_path(854)
+      expect(response).to have_http_status(:success)
+    end
   end
 end
